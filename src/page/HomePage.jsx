00c70d5e451c35cf7adff7ab0ef184c56c4bb4fc
Michael-Pellizzato <pellizzato.michael97@gmail.com>
2025-03-03 22:34:09 +0100

    finito esercizio

---
+++ src/page/HomePage.jsx
@@ -0,0 +1,13 @@
+const HomePage = () => {
+    return (
+        <>
+            <main className="viewport">
+                <div className="container">
+                    <h1 className="text-center">Hello world</h1>
+                </div>
+            </main>
+        </>
+    );
+};
+
+export default HomePage;
